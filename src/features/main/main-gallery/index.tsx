960de798e270b0131d7dd785c32d2055daa6c805
m.razzakberganov <m.razzakberganov@ssd.uz>
2025-10-07 11:31:41 +0500

    add changes

--- src/features/main/main-gallery/index.tsx
+++ src/features/main/main-gallery/index.tsx
@@ -1,12 +1,20 @@
+import { Carousel } from "@mantine/carousel"
 import { Button, Text } from "@mantine/core"
+import { useMediaQuery } from "@mantine/hooks"
 import cx from "clsx"
+import Autoplay from "embla-carousel-autoplay"
 import Image from "next/image"
 import { useRouter } from "next/router"
-import React from "react"
+import React, { useRef } from "react"
 
 import IconRight from "@/shared/assets/images/icons/icon-right.svg"
+import ImageEight from "@/shared/assets/images/image-eight.jpg"
+import ImageFive from "@/shared/assets/images/image-five.jpg"
 import ImageFour from "@/shared/assets/images/image-four.jpg"
+import ImageNine from "@/shared/assets/images/image-nine.jpg"
 import ImageOne from "@/shared/assets/images/image-one.jpg"
+import ImageSeven from "@/shared/assets/images/image-seven.jpg"
+import ImageSix from "@/shared/assets/images/image-six.jpg"
 import ImageThree from "@/shared/assets/images/image-three.jpg"
 import ImageTwo from "@/shared/assets/images/image-two.jpg"
 
@@ -14,6 +22,9 @@ import s from "./styles.module.scss"
 
 export const MainGallery = () => {
   const router = useRouter()
+  const matches = useMediaQuery("(max-width: 576px)")
+
+  const autoplay = useRef(Autoplay({ delay: 700, stopOnInteraction: false }))
   return (
     <>
       <div className={s.sectionWrapper}>
@@ -30,10 +41,51 @@ export const MainGallery = () => {
 
         <div className={s.cards}>
           <div className={s.topRectangle} />
-          <Image className={s.image} src={ImageOne} alt={"ImageOne"} />
-          <Image className={s.image} src={ImageTwo} alt={"ImageTwo"} />
-          <Image className={s.image} src={ImageThree} alt={"ImageThree"} />
-          <Image className={s.image} src={ImageFour} alt={"ImageFour"} />
+
+          <Carousel
+            loop
+            align="start"
+            height={400}
+            withControls={false}
+            slideGap={matches ? 12 : 20}
+            slideSize={matches ? 200 : 320}
+            plugins={[autoplay.current]}
+            onMouseEnter={autoplay.current.stop}
+            onMouseLeave={autoplay.current.reset}
+          >
+            <Carousel.Slide>
+              <Image className={s.image} src={ImageOne} alt={"ImageOne"} />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <Image className={s.image} src={ImageTwo} alt={"ImageTwo"} />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <Image className={s.image} src={ImageThree} alt={"ImageThree"} />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <Image className={s.image} src={ImageFour} alt={"ImageFour"} />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <Image
+                className={cx(s.image, s.ImageFive)}
+                src={ImageFive}
+                alt={"ImageFive"}
+              />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <Image className={s.image} src={ImageSix} alt={"ImageSix"} />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <Image className={s.image} src={ImageSeven} alt={"ImageSeven"} />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <Image className={s.image} src={ImageEight} alt={"ImageEight"} />
+            </Carousel.Slide>
+            <Carousel.Slide>
+              <Image className={s.image} src={ImageNine} alt={"ImageNine"} />
+            </Carousel.Slide>
+          </Carousel>
+
           <div className={s.bottomRectangle} />
         </div>
         {(router.pathname === "/" || router.pathname === "/main") && (
