9837ff97e25288f3c16cab91de3c6c7a005e5638
doniyor11 <udoniyorbek11@gmail.com>
2025-10-08 09:35:24 +0500

    07 10 25

--- src/features/main/books/index.tsx
+++ src/features/main/books/index.tsx
@@ -6,8 +6,8 @@ import React from "react"
 import ImageBook4 from "@/shared/assets/images/aqlvoy_oshpaz.jpg"
 import ImageBook3 from "@/shared/assets/images/book-image-3.png"
 import ImageRight from "@/shared/assets/images/icons/icon-right.svg"
-import ImageBook2 from "@/shared/assets/images/quyoshli_365_kun.jpg"
 import ImageBook5 from "@/shared/assets/images/o'zbek_book.png"
+import ImageBook2 from "@/shared/assets/images/quyoshli_365_kun.jpg"
 
 import s from "./styles.module.scss"
 
@@ -31,25 +31,41 @@ export const Books = () => {
       >
         <Carousel.Slide>
           <div className={s.card}>
-            <Image className={s.image} src={ImageBook2} alt={"Quyoshli 365 kun - Bahriddin chustiy"} />
+            <Image
+              className={s.image}
+              src={ImageBook2}
+              alt={"Quyoshli 365 kun - Bahriddin chustiy"}
+            />
             <Text>Quyoshli 365 kun</Text>
           </div>
         </Carousel.Slide>
         <Carousel.Slide>
           <div className={s.card}>
-            <Image className={s.image} src={ImageBook3} alt={"Non - Bahriddin chustiy"} />
+            <Image
+              className={s.image}
+              src={ImageBook3}
+              alt={"Non - Bahriddin chustiy"}
+            />
             <Text>Non</Text>
           </div>
         </Carousel.Slide>
         <Carousel.Slide>
           <div className={s.card}>
-            <Image className={s.image} src={ImageBook4} alt={"Bolalar taomlari - Bahriddin chustiy"} />
+            <Image
+              className={s.image}
+              src={ImageBook4}
+              alt={"Bolalar taomlari - Bahriddin chustiy"}
+            />
             <Text>Bolalar taomlari</Text>
           </div>
         </Carousel.Slide>
         <Carousel.Slide>
           <div className={s.card}>
-            <Image className={s.image} src={ImageBook5} alt={"O'zbegim - Bahriddin chustiy"} />
+            <Image
+              className={s.image}
+              src={ImageBook5}
+              alt={"O'zbegim - Bahriddin chustiy"}
+            />
             <Text>O'zbegim</Text>
           </div>
         </Carousel.Slide>
