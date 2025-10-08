80d51f1e67d6371c626fe884c3816f2b96a79faf
doniyor11 <udoniyorbek11@gmail.com>
2025-10-08 14:53:52 +0500

    08 10 25

--- src/pages/main/index.page.tsx
+++ src/pages/main/index.page.tsx
@@ -15,7 +15,14 @@ const HomePage = () => {
   return (
     <>
       <Head>
-        <title>chustiy.uz - Asosiy </title>
+        <title>Chustiy.uz: O'zbek Milliy Taomlari, Retseptlar va Pazandalik Sirlari</title>
+          <meta name="description" content="Chustiy.uz - Chust va O'zbekistonning betakror taomlari, eng sara retseptlar, qadimiy madaniyat sirlari va yangiliklari."/>
+          <meta property="og:title" content="Taom bu – nafaqat ovqat, bu – tuyg'u, xotira va san'at." key="key-title" />
+          <meta property="og:url" content="https://chustiy.uz/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="https://www.chustiy.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmain-image-1.1ffaded4.png&w=640&q=75" />
+          <meta property="og:description" content="Chustiy.uz - O'zbek Milliy Taomlari, Retseptlar va Pazandalik Sirlari." />
+          <link rel="canonical" href="https://chustiy.uz/" />
       </Head>
       <main className={"container"}>
         <MainBanner />
