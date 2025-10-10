1e9d6275a60ef90bfd9d6859e322dc4a155932c3
doniyor11 <udoniyorbek11@gmail.com>
2025-10-10 14:46:49 +0500

    10 10 25

--- src/pages/projects/shops/index.page.tsx
+++ src/pages/projects/shops/index.page.tsx
@@ -1,40 +1,40 @@
 import Head from "next/head"
 import React from "react"
 
-import { Contacts } from "@/widgets"
+import {Contacts} from "@/widgets"
 
-import { ProjectsInfo } from "@/features/projects"
+import {ProjectsInfo} from "@/features/projects"
 
-import { LocationInfo } from "@/entities"
+import {LocationInfo} from "@/entities"
 
 import ImageOne from "@/shared/assets/images/image-about-1.png"
-import { HeadBanner } from "@/shared/ui"
+import {HeadBanner} from "@/shared/ui"
 
 const ShopsPage = () => {
-  return (
-    <>
-      <Head>
-        <title>chustiy.uz - Do’konlar </title>
-      </Head>
-      <main className={"container"}>
-        <HeadBanner
-          title={"Do’konlar"}
-          subtitle={
-            "Bu mijozlar buyurtmasiga ko‘ra turli tadbirlar uchun (to‘y, yubiley, korporativ, seminar, tug‘ilgan kun va h.k.) joyida yoki oldindan tayyorlab taom pishiradigan professional oshpazdir."
-          }
-        />
-        <ProjectsInfo
-          image={ImageOne}
-          title={"Do’konlar"}
-          description={
-            "Men — oshpazlikka oshno bo‘lgan, har bir taomni qalbdan his qilib tayyorlaydigan shaxsman. Yigirma besh yildan ortiq tajribam davomida turli oshxonalar bilan ishladim: milliy an’anaviy taomlardan tortib, zamonaviy fusion yo‘nalishlarigacha. Har bir mehmon uchun shaxsiy yondashuv va mukammal ta’m — mening ustuvor vazifamdir."
-          }
-        />
-        <LocationInfo />
-      </main>
-      <Contacts />
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>chustiy.uz - Do’konlar </title>
+            </Head>
+            <main className={"container"}>
+                <HeadBanner
+                    title={"Do’konlar"}
+                    subtitle={
+                        "Bu mijozlar buyurtmasiga ko‘ra turli tadbirlar uchun (to‘y, yubiley, korporativ, seminar, tug‘ilgan kun va h.k.) joyida yoki oldindan tayyorlab taom pishiradigan professional oshpazdir."
+                    }
+                />
+                <ProjectsInfo
+                    image={ImageOne}
+                    title={"Do’konlar"}
+                    description={
+                        "Men 25 yildan ortiq oshpazlik va restoran sohasida faoliyat yuritib kelaman, shu kungacha yuzlab tadbirlarni o'tkazganman, va tajribamdan kelib chiqgan holda aytamanki HoReCa yo'nalishida ishlaydigan har bir xodimning ko'rinishi va ustidagi kiyimi juda ham muhim, ko'p yillar davomida kiyim tiktirish biz uchun ozmi ko'pmi muammollar tug'dirgan shuning uchun o'zimizning \"Zapara\" brendimizga asos soldik!"
+                    }
+                />
+                <LocationInfo/>
+            </main>
+            <Contacts/>
+        </>
+    )
 }
 
 export default ShopsPage
