e47ebfb13f4edb142df456b1b7761d346e5649ec
doniyor11 <udoniyorbek11@gmail.com>
2025-10-10 10:03:32 +0500

    10 10 25

--- src/features/about/partners/index.tsx
+++ src/features/about/partners/index.tsx
@@ -1,60 +1,75 @@
-import { Carousel } from "@mantine/carousel"
-import { Text } from "@mantine/core"
-import { useMediaQuery } from "@mantine/hooks"
+import {Carousel} from "@mantine/carousel"
+import {Text} from "@mantine/core"
+import {useMediaQuery} from "@mantine/hooks"
 import Autoplay from "embla-carousel-autoplay"
-import Image from "next/image"
-import React, { useRef } from "react"
+import Image, {StaticImageData} from "next/image"
+import React, {memo, useMemo, useRef} from "react"
 
-import ImageFive from "@/shared/assets/images/artel_logo.png"
-import ImageTwo from "@/shared/assets/images/Hilton-logo.png"
-import ImageOne from "@/shared/assets/images/mursak-logo.png"
-import ImageSex from "@/shared/assets/images/nasa-logo.png"
-import ImageThree from "@/shared/assets/images/nasa-logo.png"
-import ImageFour from "@/shared/assets/images/Uzbekistan_Turizm.png"
+import ImageArtel from "@/shared/assets/images/artel_logo.png"
+import ImageHilton from "@/shared/assets/images/Hilton-logo.png"
+import ImageMursak from "@/shared/assets/images/mursak-logo.png"
+import ImageNasa from "@/shared/assets/images/nasa-logo.png"
+import ImageTurizm from "@/shared/assets/images/Uzbekistan_Turizm.png"
 
 import s from "./styles.module.scss"
 
-export const Partners = () => {
-  const matches = useMediaQuery("(max-width: 992px)")
-
-  const autoplay = useRef(Autoplay({ delay: 1500, stopOnInteraction: false }))
-  return (
-    <div className={s.sectionWrapper}>
-      <Text className={"section-title sm"}>
-        <span>Hamkorlar</span>
-        Biz shu kungacha ko'plab mashhur brendlar bilan birgalikda ishlaganmiz
-        va ularga o'z xizmatlarimizni taqdim qilganmiz
-      </Text>
-
-      <Carousel
-        loop={true}
-        align="start"
-        withControls={false}
-        slideGap={matches ? 12 : 20}
-        slideSize={matches ? 200 : 243}
-        plugins={[autoplay.current]}
-        onMouseEnter={autoplay.current.stop}
-        onMouseLeave={autoplay.current.reset}
-      >
-        <Carousel.Slide>
-          <Image src={ImageOne} alt="Mursak" className={s.image} />
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <Image src={ImageTwo} alt="Hilton" className={s.image} />
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <Image src={ImageThree} alt="Nasa" className={s.image} />
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <Image src={ImageFour} alt="Uzbekistan Turizm" className={s.image} />
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <Image src={ImageFive} alt="Artel" className={s.image} />
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <Image src={ImageSex} alt="6" className={s.image} />
-        </Carousel.Slide>
-      </Carousel>
-    </div>
-  )
+interface Partner {
+    id: string
+    src: StaticImageData
+    alt: string
 }
+
+const PARTNERS_DATA: Partner[] = [
+    {id: "mursak", src: ImageMursak, alt: "Mursak"},
+    {id: "hilton", src: ImageHilton, alt: "Hilton"},
+    {id: "nasa", src: ImageNasa, alt: "Nasa"},
+    {id: "turizm", src: ImageTurizm, alt: "Uzbekistan Turizm"},
+    {id: "artel", src: ImageArtel, alt: "Artel"},
+    {id: "mursak", src: ImageMursak, alt: "Mursak"},
+    {id: "hilton", src: ImageHilton, alt: "Hilton"},
+    {id: "nasa", src: ImageNasa, alt: "Nasa"},
+    {id: "turizm", src: ImageTurizm, alt: "Uzbekistan Turizm"},
+    {id: "artel", src: ImageArtel, alt: "Artel"},
+]
+
+export const Partners = memo(() => {
+    const isMobile = useMediaQuery("(max-width: 992px)")
+    const autoplay = useRef(Autoplay({delay: 1500, stopOnInteraction: false}))
+
+    const carouselConfig = useMemo(
+        () => ({
+            slideGap: isMobile ? 12 : 20,
+            slideSize: isMobile ? 200 : 243,
+        }),
+        [isMobile]
+    )
+
+    return (
+        <div className={s.sectionWrapper}>
+            <Text className="section-title sm">
+                <span>Hamkorlar</span>
+                Biz shu kungacha ko'plab mashhur brendlar bilan birgalikda ishlaganmiz
+                va ularga o'z xizmatlarimizni taqdim qilganmiz
+            </Text>
+
+            <Carousel
+                loop
+                align="start"
+                withControls={false}
+                slideGap={carouselConfig.slideGap}
+                slideSize={carouselConfig.slideSize}
+                plugins={[autoplay.current]}
+                onMouseEnter={autoplay.current.stop}
+                onMouseLeave={autoplay.current.reset}
+            >
+                {PARTNERS_DATA.map((partner) => (
+                    <Carousel.Slide key={partner.id}>
+                        <Image src={partner.src} alt={partner.alt} className={s.image}/>
+                    </Carousel.Slide>
+                ))}
+            </Carousel>
+        </div>
+    )
+})
+
+Partners.displayName = "Partners"
--- src/features/services/comments/index.tsx
+++ src/features/services/comments/index.tsx
@@ -1,8 +1,8 @@
-import { Carousel } from "@mantine/carousel"
-import { Box, Flex, Text } from "@mantine/core"
-import { useMediaQuery } from "@mantine/hooks"
-import Image from "next/image"
-import React from "react"
+import {Carousel} from "@mantine/carousel"
+import {Box, Flex, Text} from "@mantine/core"
+import {useMediaQuery} from "@mantine/hooks"
+import Image, {StaticImageData} from "next/image"
+import React, {memo, useMemo} from "react"
 
 import ImageAvatar1 from "@/shared/assets/images/comments/Hakim_Ganiev.jpg"
 import ImageAvatar2 from "@/shared/assets/images/comments/Murod_Nazarov.jpg"
@@ -12,107 +12,99 @@ import ImageRight from "@/shared/assets/images/icons/icon-right.svg"
 
 import s from "./styles.module.scss"
 
-export const Comments = () => {
-  const matches = useMediaQuery("(max-width: 992px)")
-
-  return (
-    <div className={s.sectionWrapper}>
-      <Text className={"section-title sm"}>Ishonch bilan aytilgan so‘zlar</Text>
-
-      <Carousel
-        loop
-        height={matches ? 332 : 450}
-        slideGap={24}
-        slideSize={matches ? "100%" : 528}
-        align={matches ? "start" : "center"}
-        slidesToScroll={"auto"}
-        classNames={{
-          controls: s.carouselControls,
-          control: s.carouselControl,
-        }}
-        nextControlIcon={<ImageRight />}
-        previousControlIcon={<ImageRight />}
-      >
-        <Carousel.Slide>
-          <div className={s.card}>
-            <IconQuotes />
-            <Text className={s.description}>
-              "Ijtimoiy tarmoqlar orqali juda ham ko'p do'st ortirganman, shu
-              qatorda Bahriddin Chustiy bilan ham birinchi online tanishganmiz,
-              yozgan postlari va fikrlari bilan juda ham katta qiziqish
-              uyg'otgan va Bahriddin akani kuzatishni boshlaganman"
-            </Text>
-            <div className={s.cardBottom}>
-              <Box className={s.cardBottomImg}>
-                <Image
-                  className={s.image}
-                  src={ImageAvatar3}
-                  alt={"Zafar Khashimov"}
-                  width={60}
-                  height={60}
-                />
-              </Box>
-              <Flex className={s.info}>
-                <Text className={s.name}>Zafar Khashimov</Text>
-                {/*<Rating value={5} size="md" color={"#FFC500"} />*/}
-              </Flex>
-            </div>
-          </div>
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <div className={s.card}>
-            <IconQuotes />
-            <Text className={s.description}>
-              "Bu insonni ijtomoiy tarmoqlarda ko'rib uni san'ati umuman o'zbek
-              milliy taomlariga yondashuvini ko'rib qiziqish bildirib yurgan
-              edim, keyin esa Bahriddin ochgan restoranlardan biriga Chustiy
-              cuisine, keyin Sal-Sal ga borib haqiqatdan bu bolani mehri umuman
-              san'atiga nisbatdan yondashuvi o'zgachaligini ko'rdim
-            </Text>
-            <div className={s.cardBottom}>
-              <Box className={s.cardBottomImg}>
-                <Image
-                  className={s.image}
-                  src={ImageAvatar2}
-                  alt={"Murod Nazarov"}
-                  width={60}
-                  height={60}
-                />
-              </Box>
-              <Flex className={s.info}>
-                <Text className={s.name}>Murod Nazarov</Text>
-                {/*<Rating value={5} size="md" color={"#FFC500"} />*/}
-              </Flex>
-            </div>
-          </div>
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <div className={s.card}>
-            <IconQuotes />
-            <Text className={s.description}>
-              "Bahriddin ser g'ayrat va tadbirkorlik qobiliyatiga ham ega inson.
-              O'zbekistonda yangi uslubda ishlayotgn oshxonalarni ochib
-              yoshlarga o'rnak bo'lib kelmoqda. O'zbek taomlari haqida internet
-              tarmoqlarda ajoyib qilib so'zlamoqda"
-            </Text>
-            <div className={s.cardBottom}>
-              <Box className={s.cardBottomImg}>
-                <Image
-                  className={s.image}
-                  src={ImageAvatar1}
-                  alt={"Xakim Ganiev"}
-                  width={60}
-                  height={60}
-                />
-              </Box>
-              <Flex className={s.info}>
-                <Text className={s.name}>Xakim Ganiev</Text>
-                {/*<Rating value={5} size="md" color={"#FFC500"} />*/}
-              </Flex>
-            </div>
-          </div>
-        </Carousel.Slide>
-      </Carousel>
-    </div>
-  )
+interface CommentData {
+    id: string
+    text: string
+    author: string
+    avatar: StaticImageData
 }
+
+const COMMENTS_DATA: CommentData[] = [
+    {
+        id: "zafar",
+        text: "Ijtimoiy tarmoqlar orqali juda ham ko'p do'st ortirganman, shu qatorda Bahriddin Chustiy bilan ham birinchi online tanishganmiz, yozgan postlari va fikrlari bilan juda ham katta qiziqish uyg'otgan va Bahriddin akani kuzatishni boshlaganman",
+        author: "Zafar Khashimov",
+        avatar: ImageAvatar3,
+    },
+    {
+        id: "murod",
+        text: "Bu insonni ijtomoiy tarmoqlarda ko'rib uni san'ati umuman o'zbek milliy taomlariga yondashuvini ko'rib qiziqish bildirib yurgan edim, keyin esa Bahriddin ochgan restoranlardan biriga Chustiy cuisine, keyin Sal-Sal ga borib haqiqatdan bu bolani mehri umuman san'atiga nisbatdan yondashuvi o'zgachaligini ko'rdim",
+        author: "Murod Nazarov",
+        avatar: ImageAvatar2,
+    },
+    {
+        id: "xakim",
+        text: "Bahriddin ser g'ayrat va tadbirkorlik qobiliyatiga ham ega inson. O'zbekistonda yangi uslubda ishlayotgn oshxonalarni ochib yoshlarga o'rnak bo'lib kelmoqda. O'zbek taomlari haqida internet tarmoqlarda ajoyib qilib so'zlamoqda",
+        author: "Xakim Ganiev",
+        avatar: ImageAvatar1,
+    },
+]
+
+interface CommentCardProps {
+    comment: CommentData
+}
+
+const CommentCard = memo(({comment}: CommentCardProps) => (
+    <div className={s.card}>
+        <IconQuotes/>
+        <Text className={s.description}>{comment.text}</Text>
+        <div className={s.cardBottom}>
+            <Box className={s.cardBottomImg}>
+                <Image
+                    className={s.image}
+                    src={comment.avatar}
+                    alt={comment.author}
+                    width={60}
+                    height={60}
+                />
+            </Box>
+            <Flex className={s.info}>
+                <Text className={s.name}>{comment.author}</Text>
+            </Flex>
+        </div>
+    </div>
+))
+
+CommentCard.displayName = "CommentCard"
+
+export const Comments = memo(() => {
+    const isMobile = useMediaQuery("(max-width: 992px)")
+
+    const carouselConfig = useMemo(
+        () => ({
+            height: isMobile ? 332 : 450,
+            slideSize: isMobile ? "100%" : 528,
+            align: isMobile ? "start" : "center",
+        }),
+        [isMobile]
+    )
+
+    return (
+        <div className={s.sectionWrapper}>
+            <Text className="section-title sm">Ishonch bilan aytilgan so'zlar</Text>
+
+            <Carousel
+                loop
+                height={carouselConfig.height}
+                slideGap={24}
+                slideSize={carouselConfig.slideSize}
+                align={carouselConfig.align as "start" | "center"}
+                slidesToScroll="auto"
+                classNames={{
+                    controls: s.carouselControls,
+                    control: s.carouselControl,
+                }}
+                nextControlIcon={<ImageRight/>}
+                previousControlIcon={<ImageRight/>}
+            >
+                {COMMENTS_DATA.map((comment) => (
+                    <Carousel.Slide key={comment.id}>
+                        <CommentCard comment={comment}/>
+                    </Carousel.Slide>
+                ))}
+            </Carousel>
+        </div>
+    )
+})
+
+Comments.displayName = "Comments"
